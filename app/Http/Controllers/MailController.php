9f663bdbd72531648aec03e47e360724b85704fa
Le Hai Duong <duonghaile@gmail.com>
2017-03-23 00:08:58 +0700

    update some file

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -7,4 +7,28 @@ use Illuminate\Http\Request;
 class MailController extends Controller
 {
     //
+    public function basic_email(){
+        $data = array('name'=>"GodSent", "salt"=>"tuyensinhIU");
+
+        Mail::send('mail', $data, function($message) {
+            $message->to('lhduong@hcmiu.edu.vn', 'Dr. Duong')->subject('Testing the sending mail system');
+            $message->from('lhduong@hcmiu.edu.vn', 'Le Hai Duong');
+        });
+        echo "Basic Email Sent. Check your inbox.";
+    }
+    
+    public function html_email(){
+        $data = array('name'=>"Duc Dep Trai", 'salt' =>"tuyensinhIU");
+        Mail::send('mail', $data, function($message) {
+            $message->to('ducnt@hcmiu.edu.vn', "Nguyen Tan Duc")->subject('Laravel HTML Testing Mail');
+            $message->from('lhduong@hcmiu.edu.vn', 'Le Hai Duong');
+            $message->cc('lhduong@hcmiu.edu.vn', 'Le Hai Duong');
+        });
+        echo "HTML Email Sent. Check your inbox.";
+    }
+    public function receipt_email(){
+    	$data = array('ten'=>'Nguyen Tan Duc);
+    	return View::make('mail', $data);
+    }
+
 }
---
+++ resources/views/mail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="vi" prefix="og: http://ogp.me/ns#">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="http://tuyensinh.hcmiu.edu.vn/xmlrpc.php" />
+<title>Biên nhận đăng ký dự thi kỳ thi kiểm tra năng lực - Trường Đại học Quốc Tế - Tuyển sinh đại học năm 2017</title>	
+	
+<header class="entry-header">
+    <h1 class="entry-title">Biên nhận đăng ký dự thi kỳ thi kiểm tra năng lực - kỳ tháng 05/2017</h1>
+</header>
+<p><strong>I. THÔNG TIN THÍ SINH</strong><br />
+1. Họ và tên học sinh * {{ $ten }}<br />
