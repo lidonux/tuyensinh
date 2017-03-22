8fbf4f0c8232d1e8172097eb2bada53508bcebfe
Le Hai Duong <duonghaile@gmail.com>
2017-03-23 01:27:25 +0700

    update some file

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -32,7 +32,28 @@ class MailController extends Controller
     
     
     public function receipt_email(){
-	    $data = array('ten'=>'Ton Nu Quynh Hoa', 'ngay_sinh'=>'13/06/1999');
+	    $data = array('ten'=>'Ton Nu Quynh Hoa', 
+	    'ngay_sinh'=>'13/06/1999',
+	    'so_cmnd' => '032544624',
+	    'gioi_tinh'=>'Nữ',
+	    'dan_toc'=>'Kinh',
+	    'truong_lop_10'=>'Bùi Thị Xuân',
+	    'truong_lop_11'=>'Lê Quí Đôn',
+     	'truong_lop_12'=>'Lê Hồng Phong',
+     	'doi_tuong_uu_tien'=>'1',
+     	'uu_tien_khu_vuc'=>'3',
+     	'ho_khau_thuong_tru'=>'KP 6 Linh Trung, Thủ Đức',
+     	'dia_chi_lien_lac'=>'1 Lê Duẩn, Q.1',
+     	'so_dien_thoai'=>'0908038038',
+     	'dia_chi_email'=>'hoaquynh.tonnu@gmail.com',
+     	'mon1'=>'Lý',
+     	'mon2'=>'Hoá',
+     	'mon3'=>'Anh văn',
+     	'ngon_ngu'=>'Đề thi bằng Tiếng Việt (trừ đề thi môn Tiếng Anh)',
+     	'nganh1'=>'Công Nghệ Thông Tin (Chương trình trong nước) (Mã ngành D480201)',
+     	'nganh2'=>'Quản Trị Kinh Doanh (Chương trình trong nước) (Mã ngành D340101)',
+     	'nganh3'=>'Kỹ Thuật Điện Tử, Truyền Thông (Chương trình trong nước) (Mã ngành D520207)',
+	    );
 
         Mail::send('mail', $data, function($message) {
             $message->to('lhduong@hcmiu.edu.vn', 'Dr. Duong')->subject('Bien nhan dang ky thi DHQT');
--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
 <html lang="vi" prefix="og: http://ogp.me/ns#">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="http://tuyensinh.hcmiu.edu.vn/xmlrpc.php" />
-<title>Biên nhận đăng ký dự thi kỳ thi kiểm tra năng lực - Trường Đại học Quốc Tế - Tuyển sinh đại học năm 2017</title>	
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<link rel="pingback" href="http://tuyensinh.hcmiu.edu.vn/xmlrpc.php" />
+		<title>Biên nhận đăng ký dự thi kỳ thi kiểm tra năng lực - Trường Đại học Quốc Tế - Tuyển sinh đại học năm 2017</title>	
+	</head>	
+
+	<header class="entry-header">
+    	<h1 class="entry-title">Biên nhận đăng ký dự thi kỳ thi kiểm tra năng lực - kỳ tháng 05/2017</h1>
+	</header>
+
+	<body>
+		<p><strong>I. THÔNG TIN THÍ SINH</strong><br />
+		1. Họ và tên học sinh: {{ $ten }} <br />
+		2. Ngày sinh: {{ $ngay_sinh}} <br />
+		3. Số CMND: {{ $so_cmnd }} <br />
+		4. Giới tính: {{ $gioi_tinh }} <br />
+		5. Dân tộc: {{ $dan_toc }} <br />
+		6. Nơi học PTTH: <br />
+		- Lớp 10: {{ $truong_lop_10 }} <br />
+		- Lớp 11: {{ $truong_lop_11 }} <br />
+		- Lớp 12: {{ $truong_lop_12 }} <br />
+		7. Đối tượng ưu tiên (nếu có): {{ $doi_tuong_uu_tien }} <br />
+		8. Ưu tiên khu vực (nếu có): {{ $uu_tien_khu_vuc }} <br />
+		9. Hộ khẩu thường trú: {{ $ho_khau_thuong_tru }} <br />
+		10. Địa chỉ liên lạc: {{ $dia_chi_lien_lac }} <br />
+		11. Số điện thoại: {{ $so_dien_thoai }} <br />
+		12. Email: {{ $dia_chi_email }} <br />
 	
-<header class="entry-header">
-    <h1 class="entry-title">Biên nhận đăng ký dự thi kỳ thi kiểm tra năng lực - kỳ tháng 05/2017</h1>
-</header>
-<p><strong>I. THÔNG TIN THÍ SINH</strong><br />
-1. Họ và tên học sinh: {{ $ten }}
-2. Ngày sinh: {{ $ngay_sinh}}
+		<p><strong>II. ĐĂNG KÝ DỰ THI</strong><br />
+		1. Môn bắt buộc: Toán<br />
+		2. Môn tự chọn: <br />
+		Môn 1: {{ $mon1 }}<br />
+		Môn 2: {{ $mon3 }}<br />
+		Môn 3: {{ $mon2 }}<br />
+		3. Ngôn ngữ đề thi: {{ $ngon_ngu }} <br />
+
+		<p><strong>III. NGÀNH ĐĂNG KÝ XÉT TUYỂN</strong><br />
+		1. Ngành nguyện vọng 1: {{ $nganh1 }}<br />
+		2. Ngành nguyện vọng 2: {{ $nganh2 }}<br />
+		3. Ngành nguyện vọng 3: {{ $nganh3 }}<br />
+		<!-- Mã xác thực an ninh: <?php Hash::make('plain-text'); ?> -->
+	</body>
+</html>
